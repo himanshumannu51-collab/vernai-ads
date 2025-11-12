0231864b9be64a9dcb5d11bc75a29131f40c4e08
himanshumannu51-collab <himanshu.mannu51@gmail.com>
2025-11-12 14:55:09 +0530

    Update AdCard.js

--- components/AdCard.js
+++ components/AdCard.js
@@ -1,7 +1,7 @@
 export default function AdCard({ ad }) {
   return (
-    <div className="bg-white rounded-xl shadow-md p-6 border border-gray-100">
-      <pre className="whitespace-pre-wrap font-sans text-sm leading-relaxed">
+    <div className="bg-white rounded-xl shadow-lg p-6 border border-gray-100 hover:shadow-xl transition-all">
+      <pre className="whitespace-pre-wrap font-sans text-sm leading-relaxed text-gray-800">
         {ad}
       </pre>
     </div>
