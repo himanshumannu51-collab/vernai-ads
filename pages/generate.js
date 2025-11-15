bc5791b8c8bcc9ceb7357aa8dc2f15191a1c3f5b
himanshumannu51-collab <himanshu.mannu51@gmail.com>
2025-11-15 09:27:18 +0530

    Update generate.js

--- pages/generate.js
+++ pages/generate.js
@@ -1,6 +1,7 @@
-// pages/generate.js
 import dynamic from "next/dynamic";
-const AIAdGenerator = dynamic(() => import("../components/AIAdGenerator"), { ssr: false });
+const AIAdGenerator = dynamic(() => import("../components/AIAdGenerator"), {
+  ssr: false,
+});
 
 export default function GeneratePage() {
   return <AIAdGenerator />;
