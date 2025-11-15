c0b0dd976cc40a371162d46f1139ac561f467888
himanshumannu51-collab <himanshu.mannu51@gmail.com>
2025-11-15 10:12:02 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,jsx}",
     "./components/**/*.{js,jsx}",
-    "./app/**/*.{js,jsx}"
+    "./app/**/*.{js,jsx}",
   ],
   theme: {
     extend: {},
